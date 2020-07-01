fcc042792e45033ae6f655ed2bad3a17b9071abf
SaqibArsalan <saqibarsalan1997@gmail.com>
2020-07-01 13:34:25 +0500

    Second commit, add and delete api created!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 const task = require('./routes/toDo');
 const bodyParser = require('body-parser');
-
+require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,9 +17,8 @@ app.get('/', (req, res) => {
 
 
 
-mongoose.connect(process.env.DB_CONNECTION,{useUnifiedTopology: true, useNewUrlParser: true}, () => 
-console.log("connected to db1")
-);
-
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to db"))
+.catch(err => console.log(`Could not Connected to db ${process.env.DB_CONNECTION} `, err));
 
 app.listen(3000);
--- routes/toDo.js
+++ routes/toDo.js
@@ -5,16 +5,71 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({extended:true}));
 
+//all tasks
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await ToDo.find();
+        res.json(tasks);
+    } catch (err) {
+        res.status(400).json({
+            message: err
+        });
 
-router.get('/', (req, res) => {
-    res.send("we are on To Do");
+    }
 });
 
-router.post('/', (req,res) => {
-    console.log(req.body);
-    console.log("hello world");
-    res.send(req.body);
+router.post('/', async (req,res) => {
+    console.log("body is ", req.body);
+    console.log("title", req.body.title);
+    const task = new ToDo(req.body);
+
+    try {
+        const savedTask = await task.save();
+        res.status(200).json(savedTask);
+    } catch (err) {
+        res.status(404).json({
+            message: err
+        });
+    }
 });
 
+//specific task
+router.get('/:taskId', async (req,res) => {
+    try {
+    const task = await ToDo.findById(req.params.taskId);
+    res.json(task);
+    } catch(err) {
+        res.json({
+            message: err
+        });
+    }
+});
 
+//delete specific task
+
+router.delete('/:taskId', async (req,res) => {
+    try {
+        const task = await ToDo.deleteOne({_id: req.params.taskId});
+        res.json(task);
+    } catch(err) {
+        res.json({
+            message: err
+        });
+    }
+});
+
+//update a task
+
+router.patch('/:taskId', async (req,res) => {
+    try {
+        const task = await ToDo.updateOne({_id: req.params.taskId}, { $set: {title: req.body.title} });
+        res.json(task);
+    } catch (err) {
+        res.json({
+            messsage: err
+
+        });
+
+    }
+})
 module.exports = router;
