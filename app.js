f5e0bd812600d107e575b5136ea72faec9a9b79b
SaqibArsalan <saqibarsalan1997@gmail.com>
2020-07-02 17:55:19 +0500

    final touches

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ app.use(passport.initialize());
 app.use(cookieParser());
 
 
-
-app.get('/', (req, res) => {
-    res.send("we are at home");
-});
-
 app.use(logger('dev'));
 
 app.use(express.static('static_files'));
@@ -44,6 +39,6 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 .then(() => console.log("Connected to db"))
 .catch(err => console.log(`Could not Connected to db ${process.env.DB_CONNECTION} `, err));
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
